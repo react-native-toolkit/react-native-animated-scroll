97ab87a845f95b7984f8fb9faa1a03388c05b38f
DaniAkash <s.daniakash@gmail.com>
2020-09-09 21:12:40 +0530

    docs: created example app

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,25 +1,36 @@
 import * as React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
-import AnimatedScroll from 'react-native-animated-scroll';
-
-export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    AnimatedScroll.multiply(3, 7).then(setResult);
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
-  );
-}
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import AnimatedScroll, {
+  AnimatedHeaderBackground,
+  AnimatedHeaderForeground,
+} from 'react-native-animated-scroll';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  safeAreaContainer: { flex: 1 },
 });
+
+export default function App() {
+  const list = [...Array(150).keys()];
+  return (
+    <SafeAreaView style={styles.safeAreaContainer}>
+      <AnimatedScroll
+        backgroundHeader={(props) => (
+          <AnimatedHeaderBackground
+            image={{
+              uri:
+                'https://unsplash.com/photos/wE7iXvGMPhI/download?force=true&w=2400',
+            }}
+            {...props}
+          />
+        )}
+        foregroundHeader={(props) => (
+          <AnimatedHeaderForeground title={'Animated Scroll'} {...props} />
+        )}
+      >
+        {list.map((each) => {
+          return <Text>{each}</Text>;
+        })}
+      </AnimatedScroll>
+    </SafeAreaView>
+  );
+}
